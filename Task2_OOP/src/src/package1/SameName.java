39fc77b0c14b78262f55d9d669a94ca86013b39f
Anna Bocharova <120383629+annabocharovaua@users.noreply.github.com>
2023-08-25 19:05:13 +0300

    Introduction to OOP
    Introduction to OOP

---
+++ Task2_OOP/src/src/package1/SameName.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class SameName {
+    public void print() {
+        System.out.println("Class SameName from package1");
+    }
+}
---
+++ Task2_OOP/src/src/package2/SameName.java
@@ -0,0 +1,7 @@
+package package2;
+
+public class SameName {
+    public void print() {
+        System.out.println("Class SameName from package2");
+    }
+}
