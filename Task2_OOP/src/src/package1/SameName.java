685799fd2e2a6721a5b26809672ad833cdc307c9
Anna Bocharova <120383629+annabocharovaua@users.noreply.github.com>
2023-08-25 19:06:16 +0300

    Delete Task2_OOP/src directory

--- Task2_OOP/src/src/package1/SameName.java
+++
@@ -1,7 +0,0 @@
-package package1;
-
-public class SameName {
-    public void print() {
-        System.out.println("Class SameName from package1");
-    }
-}
--- Task2_OOP/src/src/package2/SameName.java
+++
@@ -1,7 +0,0 @@
-package package2;
-
-public class SameName {
-    public void print() {
-        System.out.println("Class SameName from package2");
-    }
-}
